1c5e84d1cfa8696c519c41efb694f57bd3e7bf92
chuc3 <chuc3@rpi.edu>
2014-10-18 17:16:21 -0400

    movement and collisions

---
+++ project/Assets/movementScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class movementScript : MonoBehaviour {
+	//public GameObject waypoints;
+	GameObject waypoints;
+	public List<Transform> pts;
+	int pos;
+	// Use this for initialization
+	void Start () {
+		waypoints = GameObject.FindGameObjectWithTag ("waypoints");
+		for (int x=0;x< waypoints.transform.childCount;x++)
+			pts.Add (waypoints.transform.GetChild(x).gameObject.transform);
+		pos = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pos == pts.Count)
+			Time.timeScale = 0; // puase game when reaches the end
+		else if (transform.position != pts [pos].position)
+			transform.position = Vector3.MoveTowards (transform.position,pts[pos].position,.25f	);
+		else
+			pos++;
+	}
+}
